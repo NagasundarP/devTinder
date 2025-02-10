0ae4cf66b1655f6b42ba88f0b703f4bec47ab915
NagasundarP <55275045+NagasundarP@users.noreply.github.com>
2025-02-10 12:17:37 +0530

    Delete src/config directory

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://Naga:Admin@cluster0.idbsl.mongodb.net/devTinder"
-    );
-};
-
-module.exports = connectDB;
