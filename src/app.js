2784adae9f37251c2974fc30779b2431d2d79da1
Naga P <nagap@Nagas-MacBook-Air.local>
2025-02-11 12:21:22 +0530

    db init

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Naga",
+    lastName: "Sun",
+    emailId: "naga@gmail.com",
+    password: "password",
+  });
+  try {
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Error creating the user");
+  }
+});
 
 connectDB()
   .then(() => {
